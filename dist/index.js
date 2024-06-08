e87a7df269051cb8353cdcbd5c54ec1824a0ddf4
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-08 11:07:27 +0530

    update cors options for preflight request

--- dist/index.js
+++ dist/index.js
@@ -39,7 +39,7 @@ const corsOptions = {
     exposedHeaders: ["Access-Control-Allow-Private-Network"],
 };
 app.use((0, cors_1.default)(corsOptions));
-app.options("*", (0, cors_1.default)());
+app.options("*", (0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors());
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
